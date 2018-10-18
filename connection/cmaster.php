92669160f0db7931bda1effb54d025ad6b7eb13d
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-10-19 00:42:24 +0530

    fixed conn

--- connection/cmaster.php
+++ connection/cmaster.php
@@ -4,6 +4,6 @@
 	$hostname = "placero.pearnode.com";
 	
 	// connection to the database
-	$dbhandle = mysql_connect ( $hostname, $username, $password ) or die ( "Unable to connect to Database" );
-	mysql_select_db ("placero_db", $dbhandle ) or die ( "Could not select placero_db database" );
+	$conn = mysql_connect ( $hostname, $username, $password ) or die ( "Unable to connect to Database" );
+	mysql_select_db ("placerodb", $conn ) or die ( "Could not select placerodb database" );
 ?>
