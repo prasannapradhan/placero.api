31dd70ba2569ee9b2a71109cd6cf9fb0dfce81b3
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-10-19 11:53:08 +0530

    create area cleanup

--- api/area/create.php
+++ api/area/create.php
@@ -2,30 +2,29 @@
 	header ( "Access-Control-Allow-Origin: *" );
 	include ($_SERVER ["DOCUMENT_ROOT"] . "/connection/cmaster.php");
 	
-	$area = $_POST ['area'];
-	$areaObj = json_decode ( $area );
+	$area = $_POST['area'];
+	$areaObj = json_decode($area);
+
+	$areaAddress = $areaObj->address;
+	$areaMeasure = $areaObj->measure;
+	$areaCenter = $areaObj->centerPosition;
+	$areaPositions = $areaObj->positions;
+	$areaResources = $areaObj->resources;
+	$areaPermissions = $areaObj->permissions;
+	
 	$areaArr = (array) $areaObj;
 	
-	error_log(var_export($areaArr, true));
-	
-	$deviceID = $areaArr['deviceID'];
-	$centerLong = $areaArr['center_lon'];
-	$centerLat = $areaArr['center_lat'];
-	$desc = $areaArr['desc'];
-	$name = $areaArr['name'];
-	$createdBy = $areaArr['created_by'];
-	$uniqueId = $areaArr['unique_id'];
-	$msqft = $areaArr['msqft'];
-	$address = $areaArr['address'];
-	$type = $areaArr['type'];
-	
 	$area_insert_sql = "insert into AreaMaster (deviceID,center_lon,center_lat,description,name,createdBy,uniqueId,msqft,address,type) 
-		values('$deviceID','$centerLong','$centerLat','$desc','$name','$createdBy','$uniqueId','$msqft','$address','$type')";
-	error_log($area_insert_sql);
+		values('','$areaCenter->lng','$areaCenter->lat','$areaObj->description','$areaObj->name',
+		'$areaObj->createdBy','$areaObj->uniqueId','$areaMeasure->sqFeet','$areaAddress->storable','$areaObj->type')";
+
 	mysql_query ($area_insert_sql);
 	
-	$area_share_insert_sql = "insert into AreaShare (source_user,area_id,function_codes) values ('$createdBy','$uniqueId','full_control')";
-	mysql_query($area_share_insert_sql);
+	foreach ($areaPermissions as $permission) {
+		$area_share_insert_sql = "insert into AreaShare (source_user,area_id,function_codes) 
+			values ('$permission->userId','$permission->areaId','$permission->functionCode')";
+		mysql_query($area_share_insert_sql);
+	}
 	
 	$resp = array ();
 	$resp ['status_code'] = 'SUCCESS';
