3a1c57dc0104060d588b9ee66c47e1de9a6bf722
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-10-25 22:25:43 +0530

    fixed user search

--- api/area/search/user.php
+++ api/area/search/user.php
@@ -21,7 +21,7 @@
 		$area_record['permission'] = $row;
 		mysql_free_result($area_result);
 		
-		$positions_qry = "select * from position where place_ref='$area_id'";
+		$positions_qry = "select * from position where area_ref='$area_id'";
 		$positions_result = mysql_query($positions_qry);
 		$positions_arr = array();
 		while ($position_row = mysql_fetch_object($positions_result)) {
@@ -30,7 +30,7 @@
 		$area_record['positions'] = $positions_arr;
 		mysql_free_result($positions_result);
 		
-		$resources_qry = "select * from media where place_ref ='$area_id'";
+		$resources_qry = "select * from media where area_ref ='$area_id'";
 		$resources_result = mysql_query($resources_qry);
 		$resources_arr = array();
 		while ($resource_row = mysql_fetch_object($resources_result)) {
