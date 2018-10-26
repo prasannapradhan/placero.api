bf2b06509cf5d69350f98f2b76fb30c14e99046c
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-10-26 13:54:36 +0530

    added logs

--- api/media/create.php
+++ api/media/create.php
@@ -8,6 +8,8 @@
 	$media_insert_sql = "insert into media (place_ref, name, type, tf_name, tf_path, rf_name, rf_path, lat, lng) 
 		values('$mediaObj->placeRef','$mediaObj->name','$mediaObj->type','$mediaObj->tfName','$mediaObj->tfPath',
 		'$mediaObj->rfName','$mediaObj->rfPath','$mediaObj->lat','$mediaObj->lng')";
+	
+	error_log($media_insert_sql);
 	mysql_query ($media_insert_sql);
 	
 	$media_id = mysql_insert_id();
