a5271a3cbf0f011c98318e98e44c55d8a4bdd127
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-10-26 11:32:36 +0530

    added log

--- api/position/create.php
+++ api/position/create.php
@@ -8,6 +8,7 @@
 	$pos_insert_sql = "INSERT INTO position (id, area_ref, device_id, name, description, lat, lng, tags, type, created_on) 
 		VALUES('$posObj->id', '$posObj->areaRef', '', $posObj->name', '$posObj->description', '$posObj->lat', '$posObj->lng', 
 		'$posObj->tags', '$posObj->type', '$posObj->createdOn')";
+	error_log($pos_insert_sql);
 	mysql_query($pos_insert_sql);
 	
 	$resp = array();
