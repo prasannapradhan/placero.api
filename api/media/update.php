ef2561e14104f01c92c8b3122c0330442b3fa5da
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-10-26 12:05:50 +0530

    media functions

--- api/media/update.php
+++ api/media/update.php
@@ -5,9 +5,7 @@
 	$media = $_POST['media'];
 	$mediaObj = json_decode($media);
 	
-	$media_update_sql = "update media set place_ref='$mediaObj->placeRef',name='$mediaObj->tfName',type='$mediaObj->type',
-		tf_name='$mediaObj->name',tf_path='$mediaObj->tfPath',rf_name='$mediaObj->rfName',rf_path='$mediaObj->rfPath',
-		lat='$mediaObj->lat',lng='$mediaObj->lng' where id=$mediaObj->id"; 
+	$media_update_sql = "update media set name='$mediaObj->name' where id=$mediaObj->id"; 
 	mysql_query($media_update_sql);
 	
 	$resp = array ();
