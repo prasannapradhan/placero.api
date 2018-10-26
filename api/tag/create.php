4da790f8e6a42af653a05929f9543cc05e658442
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-10-26 12:44:17 +0530

    added update for tags

---
+++ api/tag/create.php
@@ -0,0 +1,19 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	include($_SERVER["DOCUMENT_ROOT"] . "/connection/cmaster.php");
+	
+	$tag = $_POST['tag'];
+	$tagObj = json_decode($tag);
+	
+	$tag_insert_sql = "INSERT INTO tag_master (name, type, type_field, context, context_id, created_on) 
+		VALUES('$tagObj->name', '$tagObj->type', '$tagObj->typeField', '$tagObj->context', $tagObj->contextId, $tagObj->createdOn)";
+	error_log($tag_insert_sql);
+	mysql_query($tag_insert_sql);
+	
+	$resp = array();
+	$resp['status_code'] = 'SUCCESS';
+	$resp['status_msg'] = 'Tag created successfully';
+	$resp['ret_obj'] = $tag;
+	
+	echo json_encode($resp);
+?>
