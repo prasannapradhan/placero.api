3fa370a563518383cfc29e4664e3c5ea3a48f980
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-11-05 00:43:38 +0530

    fixed user create bug

--- api/user/create.php
+++ api/user/create.php
@@ -4,18 +4,23 @@
 	
 	$userObj = json_decode($_POST['user']);
 	
-	$user_del_qry = "DELETE FROM user_master where email='$userObj->email'";
-	mysql_query($user_del_qry);
+	$user_check_qry = "SELECT * from user_master where email='$userObj->email'";
+	$chk_result = mysql_query($user_check_qry);
+	$num_rows = mysql_num_rows($chk_result);
 	
-	$query = "insert into user_master (displayName, email , familyName , givenName , photoUrl , authSystemId , deviceID ) 
-		VALUES ('$userObj->displayName','$userObj->email','$userObj->familyName','$userObj->givenName','$userObj->photoUrl',
-		'$userObj->authSystemId','')";
-	
-	error_log($query);
-	mysql_query ($query);
-	
-	$user_id = mysql_insert_id();
-	$userObj->id = $user_id;
+	if($num_rows == 0){
+		$query = "insert into user_master (displayName, email , familyName , givenName , photoUrl , authSystemId , deviceID )
+				  VALUES ('$userObj->displayName','$userObj->email','$userObj->familyName','$userObj->givenName','$userObj->photoUrl',
+				  '$userObj->authSystemId','')";
+		
+		error_log($query);
+		mysql_query ($query);
+		
+		$user_id = mysql_insert_id();
+		$userObj->id = $user_id;
+	}else {
+		$userObj = mysql_fetch_object($chk_result);
+	}
 	
 	$resp = array();
 	$resp ['status_code'] = 'SUCCESS';
