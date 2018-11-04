1e58b44837135f106fe54013b2240b559cab2cfc
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-11-05 00:37:42 +0530

    bug fix

--- api/user/search/kv.php
+++ api/user/search/kv.php
@@ -6,12 +6,11 @@
 	$sf = $_GET ["sf"];
 	
 	$query = "select * from  user_master where $sf='$ss'";
-	$result = mysqli_query ( $conn, $query );
+	$result = mysql_query($query);
 	
 	$user_arr = array();
 	while ($user_row = mysql_fetch_object($result)) {
 		array_push($user_arr, $user_row);
 	}
-	
 	echo json_encode($user_arr);
 ?>
