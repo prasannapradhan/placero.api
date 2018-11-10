fc75aac03c92ff89f990fe42e05ee26f5a3a9949
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2018-11-10 12:05:58 +0530

    position log

--- api/position/update.php
+++ api/position/update.php
@@ -11,7 +11,9 @@
 		$posObj = $inputObj->area;
 	}
 	
-	$update_sql = "update position set description='$posObj->description', tags='$posObj->tags', type='$posObj->type' where id='$posObj->id'";
+	$update_sql = "update position set description='$posObj->description', tags='$posObj->tags', 
+				   type='$posObj->type' where id='$posObj->id'";
+	error_log($update_sql);
 	mysql_query($update_sql);
 	
 	$resp = array();
